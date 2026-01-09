a0fad69ac58360eb7e1a1d0ae44dfb1321f1c39b
vee-mass <vmassela@gmail.com>
2026-01-09 11:01:10 +0200

    w01 assignment

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path"); // Add this for safer path building
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static(path.join(__dirname, "..", "public"))); // top-level public folder
+router.use("/css", express.static(path.join(__dirname, "..", "public/css")));
+router.use("/js", express.static(path.join(__dirname, "..", "public/js")));
+router.use("/images", express.static(path.join(__dirname, "..", "public/images")));
 
 module.exports = router;
-
-
-
--- server.js
+++ server.js
@@ -2,18 +2,31 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
+ * View Engine and Layouts
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+
+/* ***********************
+ * Routes / Middleware
  *************************/
 app.use(static)
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
